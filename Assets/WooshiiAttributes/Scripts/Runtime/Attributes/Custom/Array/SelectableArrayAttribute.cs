870668abe1e4e93ef6f42f7d1872efe0003fca71
Damian_AntWorkshop <damian@ant-workshop.com>
2021-04-05 18:42:16 +0100

    Added images for readme to readme branch

--- Assets/WooshiiAttributes/Scripts/Runtime/Attributes/Custom/Array/SelectableArrayAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace WooshiiAttributes
-{
-    [AttributeUsage (AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
-    public class SelectableArrayAttribute : ArrayAttribute
-    {
-    }
-}
