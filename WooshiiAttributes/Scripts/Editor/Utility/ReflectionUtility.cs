5cc27e91b33f2d3adc376f202970c0d4b85c9f32
Wooshii <wooshiidev@gmail.com>
2023-06-03 20:20:19 +0100

    Added: More ReflectionUtility methods

--- WooshiiAttributes/Scripts/Editor/Utility/ReflectionUtility.cs
+++ WooshiiAttributes/Scripts/Editor/Utility/ReflectionUtility.cs
@@ -1,7 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using UnityEngine;
 using Object = UnityEngine.Object;
 
 namespace WooshiiAttributes
@@ -10,17 +11,7 @@ namespace WooshiiAttributes
     {
         public const BindingFlags DEFAULT_FLAGS = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static;
 
-        public static IEnumerable<Type> GetTypeSubclasses(Type _type, bool _avoidAbstract = false)
-        {
-            if (!_avoidAbstract)
-            {
-                return _type.Assembly.GetTypes ().Where (t => t.IsSubclassOf (_type));
-            }
-
-            return _type.Assembly.GetTypes ().Where (t => t.IsSubclassOf (_type) && !t.IsAbstract);
-        }
-
-        // Fields
+        // - Fields
 
         /// <summary>
         /// Get a field with the given name in the specified type
@@ -97,7 +88,7 @@ namespace WooshiiAttributes
             return (T)value; 
         }
 
-        // Methods
+        // - Methods
 
         /// <summary>
         /// Get a method with the given name in the specified type
@@ -130,8 +121,184 @@ namespace WooshiiAttributes
 
         public static IEnumerable<MethodInfo> GetMethods(Type _type, BindingFlags _flags = DEFAULT_FLAGS)
         {
-            return _type.GetMethods ();
+            return _type.GetMethods (_flags);
         }
 
+        // - Subtypes
+
+        public static IEnumerable<Type> GetTypeSubclasses(Type _type, bool _avoidAbstract = false)
+        {
+            if (!_avoidAbstract)
+            {
+                return _type.Assembly.GetTypes().Where(t => t.IsSubclassOf(_type));
+            }
+
+            return _type.Assembly.GetTypes().Where(t => t.IsSubclassOf(_type) && !t.IsAbstract);
+        }
+
+        /// <summary>
+        /// Get all subtypes for the given type.
+        /// </summary>
+        /// <param name="type">The type.</param>
+        /// <returns>Returns the subtypes found.</returns>
+        public static Type[] GetSubTypes(Type type)
+        {
+            // Cannot get subtypes if the given type is null
+
+            if (type == null)
+            {
+                Debug.Log("Cannot get subtypes for null type.");
+                return null;
+            }
+
+            // Return the subclasses of the type 
+
+            return GetTypesFromAssembly(GetAssemblyFromType(type)).Where(t => t.IsSubclassOf(type)).ToArray();
+        }
+
+        /// <summary>
+        /// Get all subtypes for the given type.
+        /// </summary>
+        /// <param name="type">The type.</param>
+        /// <param name="condition">The condition to check subtypes against.</param>
+        /// <returns>Returns the subtypes found.</returns>
+        public static Type[] GetSubTypes(Type type, Predicate<Type> condition)
+        {
+            // Cannot get subtypes if the given type is null
+
+            if (type == null)
+            {
+                Debug.Log("Cannot get subtypes for null type.");
+                return null;
+            }
+
+            return GetSubTypes(type).Where(t => condition(t)).ToArray();
+        }
+
+        /// <summary>
+        /// Get all types from all assemblies.
+        /// </summary>
+        /// <param name="type">The type.</param>
+        /// <returns>Returns the subtypes found.</returns>
+        public static Type[] GetSubTypesFromAssemblies(Type type)
+        {
+            // Cannot get subtypes if the given type is null
+
+            if (type == null)
+            {
+                Debug.Log("Cannot get subtypes for null type.");
+                return null;
+            }
+
+            Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+
+            // Loop through all assemblies found and add the found subclasses to the list
+
+            List<Type> types = new List<Type>();
+            for (int i = 0; i < assemblies.Length; i++)
+            {
+                types.AddRange(GetTypesFromAssembly(assemblies[i], t => t.IsSubclassOf(type)));
+            }
+
+            return types.ToArray();
+        }
+
+        /// <summary>
+        /// Get all types from all assemblies.
+        /// </summary>
+        /// <param name="type">The type.</param>
+        /// <returns>Returns the subtypes found.</returns>
+        public static Type[] GetSubTypesFromAssemblies(Type type, Predicate<Type> condition)
+        {
+            return GetSubTypesFromAssemblies(type).Where(t => condition(t)).ToArray();
+        }
+
+        // - All Types
+
+        /// <summary>
+        /// Get all types from an assembly.
+        /// </summary>
+        /// <param name="assembly">The assembly.</param>
+        /// <returns>Returns the types found. If the assembly given is null, so will the array returned.</returns>
+        public static Type[] GetTypesFromAssembly(Assembly assembly)
+        {
+            if (assembly == null)
+            {
+                Debug.LogError("Cannot find types from a null assembly.");
+                return null;
+            }
+
+            return assembly.GetTypes();
+        }
+
+        /// <summary>
+        /// Get all types from an assembly.
+        /// </summary>
+        /// <param name="assembly">The assembly.</param>
+        /// <returns>Returns the types found. If the assembly given is null, so will the array returned.</returns>
+        public static Type[] GetTypesFromAssembly(Assembly assembly, Predicate<Type> condition)
+        {
+            return GetTypesFromAssembly(assembly).Where(t => condition(t)).ToArray();
+        }
+
+        /// <summary>
+        /// Get all types from all assemblies.
+        /// </summary>
+        /// <param name="assembly">The assembly.</param>
+        /// <returns>Returns the types found.</returns>
+        public static Type[] GetTypesFromAssemblies()
+        {
+            Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+
+            // Loop through all assemblies found and add the found subclasses to the list
+
+            List<Type> types = new List<Type>();
+            for (int i = 0; i < assemblies.Length; i++)
+            {
+                types.AddRange(GetTypesFromAssembly(assemblies[i]));
+            }
+
+            return types.ToArray();
+        }
+
+        /// <summary>
+        /// Get all types from all assemblies.
+        /// </summary>
+        /// <param name="assembly">The assembly.</param>
+        /// <returns>Returns the types found.</returns>
+        public static Type[] GetTypesFromAssemblies(Predicate<Type> condition)
+        {
+            Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+
+            // Loop through all assemblies found and add the found subclasses to the list
+
+            List<Type> types = new List<Type>();
+            for (int i = 0; i < assemblies.Length; i++)
+            {
+                types.AddRange(GetTypesFromAssembly(assemblies[i]).Where(t => condition(t)));
+            }
+
+            return types.ToArray();
+        }
+
+        // - Assembly
+
+        /// <summary>
+        /// Get the assembly the given type is in.
+        /// </summary>
+        /// <param name="type">The type.</param>
+        /// <returns>Returns the assembly for the type. Will return null if the type is null.</returns>
+        public static Assembly GetAssemblyFromType(Type type)
+        {
+            // Cannot get subtypes if the given type is null
+
+            if (type == null)
+            {
+                Debug.Log("Cannot get subtypes for null type.");
+                return null;
+            }
+
+            return Assembly.GetAssembly(type);
+        }
     }
 }
