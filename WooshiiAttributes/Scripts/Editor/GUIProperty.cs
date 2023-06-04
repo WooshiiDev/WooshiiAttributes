3d380158d2d82e74916307756925978b74300aa2
Wooshii <wooshiidev@gmail.com>
2023-06-04 02:30:33 +0100

    Added: GUIProperty for representing a serialized property + field

---
+++ WooshiiAttributes/Scripts/Editor/GUIProperty.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reflection;
+using UnityEditor;
+
+namespace WooshiiAttributes
+{
+    /// <summary>
+    /// Representation of an exposed field.
+    /// </summary>
+    public class GUIProperty
+    {
+        /// <summary>
+        /// The original SerializedProperty that this GUIProperty is connected to.
+        /// </summary>
+        public readonly SerializedProperty SerializedValue;
+
+        /// <summary>
+        /// The FieldInfo for this property.
+        /// </summary>
+        public readonly FieldInfo Field;
+
+        /// <summary>
+        /// Create a new property instance, providing the <see cref="SerializedProperty"/> it represents.
+        /// </summary>
+        /// <param name="property">The SerializedProperty this object represents.</param>
+        public GUIProperty(SerializedProperty property)
+        {
+            SerializedValue = property;
+            Field = GetField();
+        }
+
+        /// <summary>
+        /// Get the field from the serialized property for this instance.
+        /// </summary>
+        /// <returns>Returns the field for the serialized property.</returns>
+        private FieldInfo GetField()
+        {
+            Type parent = SerializedValue.serializedObject.targetObject.GetType();
+            return ReflectionUtility.GetField(parent, SerializedValue.name);
+        }
+    }
+}
