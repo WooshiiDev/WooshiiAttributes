e3435c71e351ee10efc115f2d9e4503917d281c4
Wooshii <wooshiidev@gmail.com>
2025-12-22 21:53:38 +0000

    Removed: Interfaces & moved base drawers to the core folder

---
+++ WooshiiAttributes/Scripts/Editor/Core/GUIDrawerAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WooshiiAttributes
+{
+    public class GUIDrawerAttribute : Attribute
+    {
+        /// <summary>
+        /// The target GUIElement.
+        /// </summary>
+        public Type Element { get; }
+
+        public GUIDrawerAttribute(Type element)
+        {
+            Element = element;
+        }
+    }
+}
